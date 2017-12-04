7f09aba609cd7de4795c86bfd61d014ad766f8c0
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-04 23:15:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,7 +83,7 @@ class Student
       WHERE grade == "10"
       LIMIT ?
     SQL
-    
+
     DB[:conn].execute(sql, x)
   end
 
@@ -94,7 +94,7 @@ class Student
       WHERE grade == "10"
       LIMIT 1
     SQL
-    
+
     DB[:conn].execute(sql)
   end
 
